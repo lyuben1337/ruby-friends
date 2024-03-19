e23560003b8c62c9c894b39dce7f4e0e594e360f
lyuben1337 <lyuben1337@gmail.com>
2024-03-19 14:55:05 +0100

    added permissions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_19_122917) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_19_132035) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_19_122917) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
